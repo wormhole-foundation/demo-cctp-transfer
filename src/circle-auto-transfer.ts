7e3f47653f9ead2b06e21124a7cfe05257eba341
martin0995 <martin@papermoon.io>
2024-10-09 12:43:26 +0200

    add cctp auto script

---
+++ src/circle-auto-transfer.ts
@@ -0,0 +1,36 @@
+import { wormhole, amount } from '@wormhole-foundation/sdk';
+import evm from '@wormhole-foundation/sdk/evm';
+import solana from '@wormhole-foundation/sdk/solana';
+import { getSigner } from './helpers/helpers';
+
+(async function () {
+	const wh = await wormhole('Testnet', [evm, solana]);
+
+	const sendChain = wh.getChain('Avalanche');
+	const rcvChain = wh.getChain('Sepolia');
+
+	// Get signer from local key
+	const source = await getSigner(sendChain);
+	const destination = await getSigner(rcvChain);
+
+	const amt = 1_000_001n;
+
+	const automatic = true;
+
+	const nativeGas = automatic ? amount.units(amount.parse('0.0', 6)) : 0n;
+
+	const xfer = await wh.circleTransfer(
+		amt,
+		source.address,
+		destination.address,
+		automatic,
+		undefined,
+		nativeGas
+	);
+
+	console.log('Starting Transfer');
+	const srcTxids = await xfer.initiateTransfer(source.signer);
+	console.log(`Started Transfer: `, srcTxids);
+
+	process.exit(0);
+})();
